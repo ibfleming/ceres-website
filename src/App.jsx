20917ae80bd3b8dd1f654f541379fc9c3f859dcb
ibfleming <ianfleming678@gmail.com>
2024-05-21 16:09:44 -0700

    Re-implemented globe, more performant...

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ export default function App() {
 	return (
 		<div>
 			<Globe />
-			<nav className='flex w-full'>
+			<nav className='flex w-full mt-12'>
 				<ul className='flex w-full'>
 					<li className='flex-1'>
-						<button className='w-full tracking-wider'>Home</button>
+						<button className='w-full tracking-wider font-semibold'>
+							Home
+						</button>
 					</li>
 					<li className='flex-1'>
-						<button className='w-full tracking-wider'>Services</button>
+						<button className='w-full tracking-wider font-semibold'>
+							Services
+						</button>
 					</li>
 					<li className='flex-1'>
-						<button className='w-full tracking-wider'>Contact</button>
+						<button className='w-full tracking-wider font-semibold'>
+							Contact
+						</button>
 					</li>
 				</ul>
 			</nav>
--- src/Globe.jsx
+++ src/Globe.jsx
@@ -1,42 +1,90 @@
 // src/Globe.jsx
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useMemo, useCallback } from 'react';
 import Globe from 'react-globe.gl';
+import * as THREE from 'three';
 import './Globe.css';
 import '../src/styles/fonts.css';
 
-const GlobeComponent = () => {
-	const globeEl = useRef();
+const World = () => {
+	const globeRef = useRef();
 
-	useEffect(() => {
-		const MAP_CENTER = { lat: 10, lng: 0, altitude: 4 };
-		globeEl.current.pointOfView(MAP_CENTER, 0);
+	const MAP_CENTER = useMemo(() => ({ lat: 10, lng: 0, altitude: 2 }), []);
 
-		const controls = globeEl.current.controls();
+	const initializeGlobe = useCallback(() => {
+		const globe = globeRef.current;
+		if (!globe) return;
+
+		// Controls
+		const controls = globe.controls();
 		controls.enableRotate = false;
 		controls.autoRotate = true;
-		controls.autoRotateSpeed = 0.5;
+		controls.autoRotateSpeed = 0.33;
 		controls.enableZoom = false;
 		controls.enablePan = false;
-	}, []);
+
+		// Perspective
+		globe.pointOfView(MAP_CENTER, 0);
+
+		// Renderer
+		const renderer = globe.renderer();
+		renderer.setClearColor(0x000000, 0);
+		renderer.setPixelRatio(window.devicePixelRatio);
+		renderer.outputEncoding = THREE.sRGBEncoding;
+		renderer.shadowMap.enabled = false;
+		renderer.antialias = false;
+
+		// Clouds
+		const CLOUDS_IMG = '/src/assets/clouds.png';
+		const CLOUDS_ALT = 0.01;
+		const CLOUDS_ROTATION_SPEED = -0.005;
+
+		new THREE.TextureLoader().load(CLOUDS_IMG, (cloudsTexture) => {
+			const cloudsMaterial = new THREE.MeshPhongMaterial({
+				map: cloudsTexture,
+				transparent: true,
+				opacity: 0.25,
+				depthWrite: false,
+			});
+			const cloudsGeometry = new THREE.SphereGeometry(
+				globe.getGlobeRadius() * (1 + CLOUDS_ALT),
+				75,
+				75
+			);
+			const clouds = new THREE.Mesh(cloudsGeometry, cloudsMaterial);
+			globe.scene().add(clouds);
+
+			(function rotateClouds() {
+				clouds.rotation.y += (CLOUDS_ROTATION_SPEED * Math.PI) / 180;
+				requestAnimationFrame(rotateClouds);
+			})();
+		});
+	}, [MAP_CENTER]);
+
+	useEffect(() => {
+		initializeGlobe();
+	}, [initializeGlobe]);
 
 	return (
-		<div className='parent-wrapper drop-shadow-xl'>
-			<div className='globe-container'>
+		<div className='parent-wrapper'>
+			<div className='flex relative w-fit'>
 				<Globe
-					ref={globeEl}
+					ref={globeRef}
+					onGlobeReady={initializeGlobe}
+					width={250}
+					height={250}
+					// Images
 					globeImageUrl='src/assets/earth-detail.jpg'
 					bumpImageUrl='src/assets/earth-topology.png'
 					backgroundColor='rgba(0,0,0,0)'
 					showAtmosphere={false}
-					width={250}
-					height={500}
 					enableGlobeGlow={false}
 					enableBackground={false}
 					enableClouds={false}
+					animateIn={false}
 				/>
 			</div>
-			<div className='ml-5'>
+			<div className='ml-3'>
 				<div className='flex items-end'>
 					<h1 className='font-sofachrome italic text-secondary text-5xl'>
 						CERES,
@@ -55,4 +103,4 @@ const GlobeComponent = () => {
 	);
 };
 
-export default GlobeComponent;
+export default World;
